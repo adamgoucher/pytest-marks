d8227bd6fba8d601cc36f3fc3631cce4b1cafb34
Adam Goucher <adam@goucher.ca>
2011-06-16 22:27:32 -0400

    adding the marks decorator

---
+++ marks/marks.py
@@ -0,0 +1,25 @@
+import pytest, py
+from _pytest.mark import MarkInfo
+
+def pytest_namespace():
+    return {'marks': MarksDecorator}
+
+class MarksDecorator(object):
+    """ A decorator.  Example::
+
+         import py
+         @py.test.marks('mark_one', 'mark_two', 'mark_n')
+         def test_function():
+            pass
+
+    will set a 'slowtest' :class:`MarkInfo` object
+    on the ``test_function`` object. """
+    
+    def __init__(self, *args):
+        self.marks = args
+        
+    def __call__(self, f):
+        for mark in self.marks:
+            info = MarkInfo(mark, None, None)
+            setattr(f, mark, info)
+        return f
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="pytest-marks",
+    packages = ["marks"],
+    version = "0.1",
+    author = "adam goucher",
+    author_email = "adam@element34.ca",
+    install_requires = ['pytest>2.0.2'],
+    long_description=open('README.txt').read(),
+    
+    # the following makes a plugin available to py.test
+    entry_points = {
+        'pytest11': [
+            'marks = marks.marks',
+        ]
+    },
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+    ]
+)
